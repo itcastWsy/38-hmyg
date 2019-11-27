561693677225ffd413ca20dc57e9b0a59a1422a5
uksir <yeah126139163@163.com>
2019-11-27 11:33:18 +0800

    首页轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,34 @@
 
-Page({})
+/* 
+* 1 轮播图 
+    0 在页面的onLoad事件中来发送异步请求 
+    1 发送异步请求或者数据 ？
+    2 把数据动态渲染出来
+      1 利用 swiper标签
+      2 利用 image标签 
+! 2 
+? 3 
+
+ */
+Page({
+  data:{
+    // 轮播图数组
+    swiperList:[]
+  },
+  onLoad() {
+    // 1 发送异步请求  https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
+
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+        // 特别小心 箭头函数 和 this的关系 
+        // console.log(result.data.message);
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    });
+
+
+  }
+})
