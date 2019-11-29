bb77cb1667e8defc886c8da236346ff3a5637547
uksir <yeah126139163@163.com>
2019-11-29 11:54:23 +0800

    分类-页面的首次动态渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -11,68 +11,49 @@
 5 缓存。。 
  */
 
+import request from "../../utils/request";
+
+// 引入一个假数据
+// import  mockData from "./mock";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // wxml中 只能找到 data中的变量的数据 
+  // data 中应该只存放 视图渲染 要用到的数据  
+  // data中的数据越多 页面越卡！！！
+  // 视图要使用的全局数据
   data: {
-
+    // 分类数据
+    // 左侧的标题数组
+    // leftMenus:["大家电","海外购"....],
+    leftMenus: [],
+    // 右侧的内容 列表
+    rightGoods: []
   },
+  // 全局的内部的数据  wxml中找不到   Cates
+  // js内部要使用的全局数据 
+  Cates: [],
+  onLoad() {  
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    request({ url: "categories" })
+      .then(res => {
+        // console.log(res.data.message);
+        // this.setData({
+        //   cates:res.data.message
+        // })
 
-  },
+        this.Cates = res.data.message;
+        // console.log(this.Cates.map(v=>v.cat_name));
+        // 给左侧菜单数组赋值
+        this.setData({
+          leftMenus: this.Cates.map(v => v.cat_name),
+          // 右侧的内容
+          rightGoods:this.Cates[0].children
+        })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+        // 右侧要实现的 商品数组 
+        // console.log(this.Cates[0].children);
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      })
+    // console.log(mockData);
   }
 })
