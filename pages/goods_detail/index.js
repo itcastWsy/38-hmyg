62d45bc23292b3adae1059e4c17fad484a8a2cd2
uksir <yeah126139163@163.com>
2019-11-30 15:54:10 +0800

    商品详情-轮播图

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,34 @@
-// pages/goods_detail/index.js
+/* 
+1 发送请求获取数据 页面渲染
+ */
+
+
+
+
+import request from "../../utils/request";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsInfo:{}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getDetail(options.goods_id);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // 获取商品的详情数据
+  getDetail(goods_id) {
+    request({
+      url: "goods/detail",
+      data: {
+        goods_id
+      }
+    }).then(res => {
+      console.log(res);
+      this.setData({
+        goodsInfo:res.data.message
+      })
+    })
 
   }
 })
