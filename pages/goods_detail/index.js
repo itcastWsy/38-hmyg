afa6c80b1f79adeb935db3512f03e3b02a3880a0
uksir <yeah126139163@163.com>
2019-12-02 16:04:12 +0800

    完成添加购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -51,5 +51,48 @@ Page({
       current,
       urls
     });
+  },
+  // 点击 “加入购物车”
+  handleGoodsAdd() {
+    /* 
+    1 先获取缓存中的旧的购物车的数据  [{}] || []
+    2 先判断当前的商品是否存在于 缓存中了
+      1 没有 数组添加元素即可！
+      2 已经添加过 
+          获取到 数组的中的元素 对它的属性-购买数量 执行 ++ 即可
+    3 把数组 重新添加到缓存中 ！！ 
+     */
+
+    //  1 获取购物车的数组 在缓存中
+    let carts = wx.getStorageSync("carts") || [];
+    // 2 判断 当前的商品是否已经存在了
+    const index = carts.findIndex(v => v.goods_id === this.data.goodsInfo.goods_id);
+    // 3 判断 当前的商品是否已经存在了
+    if (index === -1) {
+      // 不存在  顺便添加一个 购买数量 属性！！
+      carts.unshift({
+        ...this.data.goodsInfo,
+        nums: 1
+      });
+    } else {
+      // 已经存在了  执行数量 ++ 
+      carts[index].nums++;
+    }
+
+    // 4 重新添加到缓存中
+    wx.setStorageSync("carts", carts);
+
+    wx.showToast({
+      title: '添加成功',
+      mask: true
+    });
+
+
+
+
+
+
+
+
   }
 })
